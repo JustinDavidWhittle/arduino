a2aee46d89729a729ee9bbbb3a9827e518955e88
JustinDavidWhittle <43297619+JustinDavidWhittle@users.noreply.github.com>
2019-01-19 15:02:51 -0500

    Add files via upload

---
+++ sketch_jan19a.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(1, OUTPUT);
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);\
+pinMode(6, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(1,HIGH);
+delay(1234);
+digitalWrite(1,LOW);
+delay(1234);
+digitalWrite(2,HIGH);
+delay(1234);
+digitalWrite(2,LOW);
+delay(1234);
+digitalWrite(3,HIGH);
+delay(1234);
+digitalWrite(3,LOW);
+delay(1234);
+}
